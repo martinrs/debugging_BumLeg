c04d3a9f5ce5d8f8d157de9b8af0a67b02ff4f7d
Martin Sørensen <maros105@gmail.com>
2024-08-30 09:56:55 +0200

    Initial commit

---
+++ buggy_bumleg.py
@@ -0,0 +1,31 @@
+def checkForBUM(tal, bumtal):
+    if tal % bumtal == 0:
+        return True
+    if str(bumtal) in str(tal):
+        return True
+    else:
+        return False
+
+bum = input('Vælg et bumtal: ')
+
+tur = 1
+
+for i in range(1, 51):
+    valg = input('Tal eller bum? ')
+
+    if checkForBUM(i, bum):
+        korrekt = 'bum'
+    else:
+        korrekt = str(i)
+
+    # Kontroller spillerens indtastning
+    if valg == korrekt:
+        print('Korrekt!')
+    else:
+        print('Forkert!')
+
+    # Skift spiller
+    if tur == 1:
+        tur = 2
+    else:
+        tur = 1
